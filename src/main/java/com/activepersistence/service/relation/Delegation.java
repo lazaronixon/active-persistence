fcb58178452b7bcf5cb6f6d3918de1c0fad3b001
Lázaro Nixon <lazaronixon@hotmail.com>
2020-06-16 09:38:59 -0300

    rename methods alt to fail.. ex findAlt findOrFail

--- src/main/java/com/activepersistence/service/relation/Delegation.java
+++ src/main/java/com/activepersistence/service/relation/Delegation.java
@@ -3,166 +3,166 @@ package com.activepersistence.service.relation;
 import com.activepersistence.service.Relation;
 import java.util.List;
 
-public interface Delegation<T> {    
-    
-    public Relation<T> buildRelation();    
+public interface Delegation<T> {
+
+    public Relation<T> buildRelation();
 
     //<editor-fold defaultstate="collapsed" desc="finder methods">
     public default T take() {
         return buildRelation().take();
     }
-    
-    public default T takeAlt() {
-        return buildRelation().takeAlt();
+
+    public default T takeOrFail() {
+        return buildRelation().takeOrFail();
     }
-    
+
     public default T first() {
         return buildRelation().first();
     }
-    
-    public default T firstAlt() {
-        return buildRelation().firstAlt();
+
+    public default T firstOrFail() {
+        return buildRelation().firstOrFail();
     }
-    
+
     public default T last() {
         return buildRelation().last();
     }
-    
-    public default T lastAlt() {
-        return buildRelation().lastAlt();
+
+    public default T lastOrFail() {
+        return buildRelation().lastOrFail();
     }
-    
+
     public default List<T> take(int limit) {
         return buildRelation().take(limit);
     }
-    
+
     public default List<T> first(int limit) {
         return buildRelation().first(limit);
     }
-    
+
     public default List<T> last(int limit) {
         return buildRelation().last(limit);
     }
-    
+
     public default T findBy(String conditions, Object... params) {
         return buildRelation().findBy(conditions, params);
     }
-    
-    public default T findByAlt(String conditions, Object... params) {
-        return buildRelation().findByAlt(conditions, params);
+
+    public default T findByOrFail(String conditions, Object... params) {
+        return buildRelation().findByOrFail(conditions, params);
     }
-    
+
     public default boolean exists() {
         return buildRelation().exists();
     }
-    
+
     public default boolean exists(String conditions, Object... params) {
         return buildRelation().exists(conditions, params);
     }
-    //</editor-fold>       
+    //</editor-fold>
 
     //<editor-fold defaultstate="collapsed" desc="query methods">
     public default Relation<T> all() {
         return buildRelation().all();
     }
-    
+
     public default Relation<T> where(String conditions, Object... params) {
         return buildRelation().where(conditions, params);
     }
-    
+
     public default Relation<T> order(String... values) {
         return buildRelation().order(values);
     }
-    
+
     public default Relation<T> limit(int value) {
         return buildRelation().limit(value);
     }
-    
+
     public default Relation<T> offset(int value) {
         return buildRelation().offset(value);
     }
-    
+
     public default Relation<T> select(String... values) {
         return buildRelation().select(values);
     }
-    
+
     public default Relation<T> joins(String... values) {
         return buildRelation().joins(values);
     }
-    
+
     public default Relation<T> group(String... values) {
         return buildRelation().group(values);
     }
-    
+
     public default Relation<T> having(String conditions, Object... params) {
         return buildRelation().having(conditions, params);
     }
-    
+
     public default Relation<T> distinct() {
         return buildRelation().distinct();
     }
-    
+
     public default Relation<T> none() {
         return buildRelation().none();
     }
-    
+
     public default Relation<T> includes(String... values) {
         return buildRelation().includes(values);
     }
-    
+
     public default Relation<T> eagerLoads(String... values) {
         return buildRelation().eagerLoads(values);
     }
-    
+
     public default Relation<T> reselect(String... values) {
         return buildRelation().reselect(values);
     }
-    
+
     public default Relation<T> rewhere(String condition, Object[] params) {
         return buildRelation().rewhere(condition, params);
     }
-    
+
     public default Relation<T> reorder(String... fields) {
         return buildRelation().reorder(fields);
     }
-    
+
     public default Relation<T> lock() {
         return buildRelation().lock();
     }
     //</editor-fold>
-    
+
     //<editor-fold defaultstate="collapsed" desc="calculation">
     public default long count() {
         return buildRelation().count();
     }
-    
+
     public default long count(String field) {
         return buildRelation().count(field);
     }
-    
+
     public default <R> R minimum(String field, Class<R> resultClass) {
         return buildRelation().minimum(field, resultClass);
     }
-    
+
     public default <R> R maximum(String field, Class<R> resultClass) {
         return buildRelation().maximum(field, resultClass);
     }
-    
+
     public default <R> R average(String field, Class<R> resultClass) {
         return buildRelation().average(field, resultClass);
     }
-    
+
     public default <R> R sum(String field, Class<R> resultClass) {
         return buildRelation().sum(field, resultClass);
     }
-    
+
     public default List pluck(String... field) {
         return buildRelation().pluck(field);
     }
-    
+
     public default List ids() {
         return buildRelation().ids();
     }
-    //</editor-fold>   
+    //</editor-fold>
 
 }
--- src/main/java/com/activepersistence/service/relation/FinderMethods.java
+++ src/main/java/com/activepersistence/service/relation/FinderMethods.java
@@ -3,20 +3,20 @@ package com.activepersistence.service.relation;
 import com.activepersistence.service.Relation;
 import java.util.List;
 
-public class FinderMethods<T> {               
-    
+public class FinderMethods<T> {
+
     private final Relation<T> relation;
 
     public FinderMethods(Relation relation) {
         this.relation = relation;
-    }        
-    
+    }
+
     public T take() {
         return relation.limit(1).fetchOne();
     }
 
-    public T takeAlt() {
-        return relation.limit(1).fetchOneAlt();
+    public T takeOrFail() {
+        return relation.limit(1).fetchOneOrFail();
     }
 
     public T first() {
@@ -27,54 +27,54 @@ public class FinderMethods<T> {
         }
     }
 
-    public T firstAlt() {
-        if (relation.getOrderValues().isEmpty()) {     
-            return relation.order("this.id").takeAlt();
+    public T firstOrFail() {
+        if (relation.getOrderValues().isEmpty()) {
+            return relation.order("this.id").takeOrFail();
         } else {
-            return relation.takeAlt();
+            return relation.takeOrFail();
         }
     }
-    
+
     public List<T> first(int limit) {
         if (relation.getOrderValues().isEmpty()) {
-            return relation.order("this.id").take(limit); 
+            return relation.order("this.id").take(limit);
         } else {
-            return relation.take(limit);   
-        }      
-    }    
+            return relation.take(limit);
+        }
+    }
 
     public T last() {
-        if (relation.getOrderValues().isEmpty()) {        
+        if (relation.getOrderValues().isEmpty()) {
             return relation.order("this.id DESC").take();
         } else {
             return relation.take();
         }
     }
 
-    public T lastAlt() {
-        if (relation.getOrderValues().isEmpty()) {        
-            return relation.order("this.id DESC").takeAlt();
+    public T lastOrFail() {
+        if (relation.getOrderValues().isEmpty()) {
+            return relation.order("this.id DESC").takeOrFail();
         } else {
-            return relation.takeAlt();
+            return relation.takeOrFail();
         }
     }
-    
+
     public List<T> last(int limit) {
         if (relation.getOrderValues().isEmpty()) {
-            return relation.order("this.id DESC").take(limit); 
+            return relation.order("this.id DESC").take(limit);
         } else {
-            return relation.take(limit);   
-        }    
-    }    
+            return relation.take(limit);
+        }
+    }
 
     public T findBy(String conditions, Object... params) {
         return relation.where(conditions, params).take();
     }
 
-    public T findByAlt(String conditions, Object... params) {
-        return relation.where(conditions, params).takeAlt();
+    public T findByOrFail(String conditions, Object... params) {
+        return relation.where(conditions, params).takeOrFail();
     }
-    
+
     public boolean exists(String conditions, Object... params) {
         return relation.where(conditions, params).exists();
     }
@@ -85,6 +85,6 @@ public class FinderMethods<T> {
 
     public List<T> take(int limit) {
         return relation.limit(limit).fetch();
-    }    
-    
+    }
+
 }
