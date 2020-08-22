e5776657e9b55fe81de796dbe7758382bf4f5067
Lázaro Nixon <lazaronixon@hotmail.com>
2020-08-22 02:28:17 -0300

    small refactor

--- src/main/java/com/activepersistence/service/Persistence.java
+++ src/main/java/com/activepersistence/service/Persistence.java
@@ -11,29 +11,29 @@ public interface Persistence<T> {
     public EntityManager getEntityManager();
 
     @Transactional
-    public default T create(com.activepersistence.model.Base entity) {
-        return flush(() -> { getEntityManager().persist(entity); return (T) entity; });
-    }
-
-    @Transactional
-    public default T update(com.activepersistence.model.Base entity) {
-        return flush(() -> (T) getEntityManager().merge(entity));
-    }
-
-    @Transactional
     public default T save(com.activepersistence.model.Base entity) {
         if (entity.isDestroyed()) return (T) entity;
 
         if (entity.isNewRecord()) {
-            return create(entity);
+            return create((T) entity);
         } else {
-            return update(entity);
+            return update((T) entity);
         }
     }
 
     @Transactional
     public default void destroy(com.activepersistence.model.Base entity) {
         flush(() -> { if (entity.isPersisted()) getEntityManager().remove(getEntityManager().merge(entity)); });
+    }
+
+    @Transactional
+    public default T create(T entity) {
+        return flush(() -> { getEntityManager().persist(entity); return entity; });
+    }
+
+    @Transactional
+    public default T update(T entity) {
+        return flush(() -> getEntityManager().merge(entity));
     }
 
     public default void reload(T entity) {
